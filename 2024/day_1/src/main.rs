12d4832c26b06d4d6844a0daa24004e8e05b9f4f
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-12-09 10:29:28 -0600

    Feat(FileRead): Implement file reading and parsing

--- 2024/day_1/src/main.rs
+++ 2024/day_1/src/main.rs
@@ -1,20 +1,21 @@
 use std::collections::BinaryHeap;
 use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
-
-fn read_lines<T>(filename: T) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    T: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
+use std::io::{self, BufRead, BufReader};
 
 fn main() {
-    if let Ok(lines) = read_lines("../data/location_ids.txt") {
-        for line in lines.flatten() {
-            println!("{}", line);
-        }
+    let file = File::open("data/location_ids.txt").expect("file not found");
+    let reader = BufReader::new(file);
+
+    let lines: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
+
+    let mut numbers_one: Vec<i64> = vec![];
+    let mut numbers_two: Vec<i64> = vec![];
+
+    println!("The last line is {}", lines[lines.len() - 1]);
+    for line in &lines[0..lines.len() - 1] {
+        let s_vec: Vec<&str> = line.split_whitespace().collect();
+        let n_one: i64 = s_vec[0].parse().unwrap();
+        let n_two: i64 = s_vec[1].parse().unwrap();
+        println!("{n_one} {n_two}");
     }
 }
