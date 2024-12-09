32eb54047582a2a94662aa4ad5934688a8aa7dad
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-12-09 10:39:12 -0600

    Feat(Distance): Calculate distance pairs using Min-Heap

--- 2024/day_1/src/main.rs
+++ 2024/day_1/src/main.rs
@@ -1,6 +1,7 @@
+use std::cmp::Reverse;
 use std::collections::BinaryHeap;
 use std::fs::File;
-use std::io::{self, BufRead, BufReader};
+use std::io::{BufRead, BufReader};
 
 fn main() {
     let file = File::open("data/location_ids.txt").expect("file not found");
@@ -16,6 +17,25 @@ fn main() {
         let s_vec: Vec<&str> = line.split_whitespace().collect();
         let n_one: i64 = s_vec[0].parse().unwrap();
         let n_two: i64 = s_vec[1].parse().unwrap();
+
+        numbers_one.push(n_one);
+        numbers_two.push(n_two);
         println!("{n_one} {n_two}");
     }
+
+    let mut heap_one = BinaryHeap::new();
+    let mut heap_two = BinaryHeap::new();
+
+    for i in 0..numbers_one.len() {
+        heap_one.push(Reverse(numbers_one[i]));
+        heap_two.push(Reverse(numbers_two[i]));
+    }
+
+    let mut dist = 0;
+
+    while !heap_one.is_empty() && !heap_two.is_empty() {
+        dist = dist + heap_one.pop().unwrap().0 + heap_two.pop().unwrap().0;
+    }
+
+    println!("The distance is {dist}");
 }
