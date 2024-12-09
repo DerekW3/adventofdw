e8458f09ad4139cd12d362bfa7b9a3baf8f0004e
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-12-09 09:52:42 -0600

    Feat(FileRead): Implement file reading with BufRead

--- 2024/day_1/src/main.rs
+++ 2024/day_1/src/main.rs
@@ -1,3 +1,20 @@
+use std::collections::BinaryHeap;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<T>(filename: T) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    T: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Ok(lines) = read_lines("../data/location_ids.txt") {
+        for line in lines.flatten() {
+            println!("{}", line);
+        }
+    }
 }
