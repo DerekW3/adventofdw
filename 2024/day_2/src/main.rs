f8c46e8104416e94a9dc3cd6f9850ea313f75730
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-12-09 21:31:52 -0600

    Fix(Slope): Fix increasing/decreasing calculation

--- 2024/day_2/src/main.rs
+++ 2024/day_2/src/main.rs
@@ -9,17 +9,20 @@ fn main() {
 
     let mut num_valid = 0;
 
-    'outer: for line in string_vec {
+    'outer: for line in &string_vec[0..string_vec.len() - 1] {
         let num_vec: Vec<i16> = line
             .split_whitespace()
             .map(|s| s.parse::<i16>().unwrap())
             .collect();
 
+        println!("{:?}", num_vec);
+        println!("-------------");
+
         let mut dp: i16 = 0;
         for i in 1..num_vec.len() {
             let diff = num_vec[i] - num_vec[i - 1];
 
-            if (diff.abs() < 1 || diff.abs() > 3) || (i > 1 && diff.signum() == dp.signum()) {
+            if (diff.abs() < 1 || diff.abs() > 3) || (i > 1 && (diff.signum() != dp.signum())) {
                 continue 'outer;
             }
 
