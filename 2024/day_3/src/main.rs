26d140cb5a86eb46a83e9fdd9ef857b3ffeecf77
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-12-10 21:58:56 -0600

    Feat(extract_mul): Initialize extract_mul function
    Function for extracting the mul() sequences from a string

--- 2024/day_3/src/main.rs
+++ 2024/day_3/src/main.rs
@@ -8,3 +8,15 @@ fn main() {
     let mut line_vec: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
     line_vec.pop();
 }
+
+fn extract_mul(str_vec: &String) -> Vec<String> {
+    let sub_vec: Vec<&str> = str_vec.split("mul(").collect();
+
+    for chunk in sub_vec {
+        if !chunk.contains(")") {
+            continue;
+        }
+    }
+
+    vec![]
+}
