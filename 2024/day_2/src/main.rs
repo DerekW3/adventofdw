b0999dd2cca64fd4b72a59694c1ee454ade923b9
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-12-10 21:02:08 -0600

    Refactor(validate_test): Extract stage validation into separate function

--- 2024/day_2/src/main.rs
+++ 2024/day_2/src/main.rs
@@ -9,25 +9,29 @@ fn main() {
 
     let mut num_valid = 0;
 
-    'outer: for line in &string_vec[0..string_vec.len() - 1] {
+    for line in &string_vec[0..string_vec.len() - 1] {
         let num_vec: Vec<i16> = line
             .split_whitespace()
             .map(|s| s.parse::<i16>().unwrap())
             .collect();
 
-        let mut dp: i16 = 0;
-        for i in 1..num_vec.len() {
-            let diff = num_vec[i] - num_vec[i - 1];
-
-            if (diff.abs() < 1 || diff.abs() > 3) || (i > 1 && (diff.signum() != dp.signum())) {
-                continue 'outer;
-            }
-
-            dp = diff;
-        }
-
         num_valid += 1;
     }
 
     println!("The number of valid tests is {num_valid}");
+}
+
+fn validate_test(nums: Vec<i16>) -> (bool, usize) {
+    let mut dp: i16 = 0;
+    for i in 1..nums.len() {
+        let diff = nums[i] - nums[i - 1];
+
+        if (diff.abs() < 1 || diff.abs() > 3) || (i > 1 && (diff.signum() != dp.signum())) {
+            return (false, i);
+        }
+
+        dp = diff;
+    }
+
+    (true, 0)
 }
