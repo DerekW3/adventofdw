3411e398cb5c3faf1fc6833643d60edaaa88f794
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-12-10 22:07:15 -0600

    Feat(validate_chunk): Implement the validate_chunk method

--- 2024/day_3/src/main.rs
+++ 2024/day_3/src/main.rs
@@ -12,11 +12,23 @@ fn main() {
 fn extract_mul(str_vec: &String) -> Vec<String> {
     let sub_vec: Vec<&str> = str_vec.split("mul(").collect();
 
-    for chunk in sub_vec {
-        if !chunk.contains(")") {
-            continue;
-        }
-    }
+    for chunk in sub_vec {}
 
     vec![]
+}
+
+fn validate_chunk(chunk: &str) -> bool {
+    let mut partials: Vec<&str> = chunk.split(",").collect();
+
+    if partials.len() != 2 {
+        return false;
+    }
+
+    partials[1] = partials[1].split(")").next().unwrap();
+
+    if partials[0].parse::<i64>().is_err() || partials[1].parse::<i64>().is_err() {
+        return false;
+    }
+
+    true
 }
