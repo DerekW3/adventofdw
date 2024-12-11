0fd312403e182012558609c0d8a416b352b0eec3
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-12-10 21:40:56 -0600

    Feat(FileParse): Parse file and collect lines

--- 2024/day_3/src/main.rs
+++ 2024/day_3/src/main.rs
@@ -1,3 +1,10 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("data/input_day3.txt").expect("File not found");
+    let reader = BufReader::new(file);
+
+    let mut line_vec: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
+    line_vec.pop();
 }
